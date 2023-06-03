c167f7145d9e4a021fdab13d56c7b49f9131da34
bmsrangel <bmsrangel@hotmail.com>
2023-06-02 21:11:11 -0300

    Initial Commit

---
+++ InstaSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InstaSwift
+//
+//  Created by Bruno Rangel on 02/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaSwift/InstaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaSwiftApp.swift
+//  InstaSwift
+//
+//  Created by Bruno Rangel on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
